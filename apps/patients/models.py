79e96c743cc5e8c1d5fa10ecd083077be6f1f6a9
SebastianVillarroel <sebastianvillarroel15102007@gmail.com>
2026-05-15 16:00:09 -0400

    Backup de cambios antes de la reestructuracion

--- apps/patients/models.py
+++ apps/patients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    species_breed = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    birth_date = models.DateField()
+    current_weight = models.FloatField()
+    owner = models.ForeignKey('owners.Owner',on_delete=models.SET_NULL,null=True)
+    physical_marks = models.CharField(max_length=255)
+    microchip_id = models.CharField(max_length=50)
+    reproductive_status = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'patient'
+
+
+class ClinicalRecords(models.Model):
+    patient = models.OneToOneField(Patient,on_delete=models.CASCADE)
+    opened_at = models.DateField()
+    allergies_history = models.TextField()
+    medical_alerts = models.TextField()
+
+    class Meta:
+        db_table= 'clinical_records'
