9c51a2513fcf2786e671f118305d1f648925711f
Oscar <ovivas24@ujap.edu.ve>
2026-05-17 00:51:09 -0400

    #op999: Endpoint para verificar usuario. https://proyecto.paezweb.dev/wp/999

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistroUsuarioView, LoginView, PanelGerenteView
+from .views import RegistroUsuarioView, LoginView, PanelGerenteView, VerificarUsuarioView
 
 urlpatterns =[
     #ruta para registrar un usuario
@@ -8,4 +8,6 @@ urlpatterns =[
     path('login',LoginView.as_view(), name = 'user-login'),
     #ruta protegida de autorizacion(solo geretes)
     path('panel-gerente/', PanelGerenteView.as_view(), name='panel-gerente'),
+    #ruta para backend 1
+    path('me/', VerificarUsuarioView.as_view(), name='user-me'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from .permissions import esGerente
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 class PanelGerenteView(APIVIew):
     #Aqui damos doble seguridad, tiene que estar logueado y tiene que ser gerente 
@@ -14,3 +16,18 @@ class PanelGerenteView(APIVIew):
             "rol": request.user.rol 
         }
         return Response(datos_sensibles, status=status.HTTP_200_OK)
+    
+class VerificarUsuarioView(APIView):
+    #autoricacion
+    Permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        #Este es el undepoint que devuelve los datos a los usuarios dueño del token
+        user =  request.user
+        return  Response({
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "rol": getattr(user, 'rol', 'sin_rol')
+        })
