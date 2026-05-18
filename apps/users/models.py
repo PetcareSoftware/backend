365a28c3c0be5a9d489343973913a4aa638f9ebb
Alejandro Avendano <avendano0730@gmail.com>
2026-05-17 20:20:23 -0400

    feat: agregado de  modelos de ClinicalStaff, Veterinarian y Appointment

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class ClinicalStaff(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='clinical_staff')
+    cargo = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Staff - Usuario ID: {self.user_id}"
+
+class Veterinarian(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='veterinarian')
+    especialidad = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Veterinario - Usuario ID: {self.user_id}"
