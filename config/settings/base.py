88addcc1098f4e5ddbe429c590bc31a95e4ce26a
rabih06 <rabihbazi06@gmail.com>
2026-05-19 12:21:51 -0400

    fix: Conflictos resueltos y agregado de admin.py de user, owner y stock

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.users',  # Nuestra app de usuarios
     'apps.owners',  # Nuestra app de propietarios
+    'apps.stock',  # Nuestra app de inventario
 ]
 
 MIDDLEWARE = [
