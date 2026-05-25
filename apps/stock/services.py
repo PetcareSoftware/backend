ab767f8a55265059f614968d306e6024bcbb0878
SebastianVillarroel <sebastianvillarroel15102007@gmail.com>
2026-05-25 12:45:25 -0400

    Implementacion control de concurrencia transaccional ACID
    Creacion de  servicio de agendamiento seguro con Pessimist Lock para evitar colisiones de citas.
    - Creacion de servicio de consumo de inventario para garantizar el descuento atómico de stock de manera correcta.

--- apps/stock/services.py
+++ apps/stock/services.py
@@ -1,1 +1,27 @@
-# Create your services here
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from apps.stock.models import SupplyBatch, ConsultationSupply
+
+def consume_supply_securely(batch_id,consultation_ide,quantity):
+    """
+    Control de concurrencia transaccional (ACID) para descontar inventario 
+    y registrar su uso en una consulta de forma atómica.
+    """
+    with transaction.atomic():
+        try:
+            batch =SupplyBatch.objects.select_for_update().get(id = batch_id)
+        except SupplyBatch.DoesNotExist:
+            raise ValidationError(f"El lote de insumo con id {batch_id} no existe.")
+        if batch.current_stock < quantity:
+            raise ValidationError(
+                f"Error de inventario: Stock insuficiente en el lote {batch.lot_number}. "
+                f"Se pidieron {quantity}, pero solo quedan {batch.current_stock} disponibles."
+            )
+        batch.current_stock -= quantity
+        batch.save()
+        register = ConsultationSupply.objects.create(
+            consultation_id = consultation_ide,
+            batch = batch_id,
+            quantity_used = quantity
+        )
+    return register
